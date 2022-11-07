83e9a80219c0d8c640111ed4994b1a66ddfddabd
trevorabel <taabel@csustudent.net>
2022-11-07 05:40:29 -0500

    implemented AES-128 & AES-192 also corrected AES-256 buffer size to 32 from 16

--- SeniorProject/aes192.py
+++ SeniorProject/aes192.py
@@ -1,4 +1,4 @@
-# AES256 encryption method in a python script to be passed to JavaFx main class
+# AES192 encryption method in a python script to be passed to JavaFx main class
 #
 # Sources
 # AES: https://pycryptodome.readthedocs.io/en/latest/src/cipher/aes.html?highlight=AES
@@ -10,42 +10,42 @@ from Crypto.Random import get_random_bytes
 # methods for genereating keys, storing keys, and retrieving keys
 
 def gen_key():
-    # generate the key needed for AES256 encryption and decryption (symmetric key)
-    key = get_random_bytes(32)
-    with open("aes256Secretkey.key", "wb") as file:
+    # generate the key needed for AES192 encryption and decryption (symmetric key)
+    key = get_random_bytes(24)
+    with open("aes192Secretkey.key", "wb") as file:
         file.write(key)
 
 def load_key():
-    return open("aes256Secretkey.key", "rb").read()
+    return open("aes192Secretkey.key", "rb").read()
 
 def encrypt_message(message,key):
     # # Steps for encryption:
-    #  1. Create Cipher instance with AES256
+    #  1. Create Cipher instance with AES192
     #  2. Store cipher instance into a nonce to be recalled for decryption later
     #  3. Write out stored cipher(nonce) to a file to be read in for decryption
     #  4. Perform encryption while creating variables for the the encryption and tag
     #  5. Write out Encryption and Tag variables to a file to be recalled for decryption of stored message
     # #
 
-    AES256cipher = AES.new(key, AES.MODE_EAX)
-    cipherStore = AES256cipher.nonce
+    AES192cipher = AES.new(key, AES.MODE_EAX)
+    cipherStore = AES192cipher.nonce
 
     # writing cipherStore/nonce out to a file to be pulled for decryption
-    with open("aes256CipherStore_nonce.txt", "wb") as file:
+    with open("aes192CipherStore_nonce.txt", "wb") as file:
         file.write(cipherStore)
         print("Export of variable cipherStore aka nonce complete. Proceeding with encryption.")
 
-    encrpyted, tag = AES256cipher.encrypt_and_digest(message)
+    encrpyted, tag = AES192cipher.encrypt_and_digest(message)
 
     # writing out created variable tag needed for validity checking while decryption to ensure authenticity of the message
-    with open("aes256EncryptedTag.txt", "wb") as file:
+    with open("aes192EncryptedTag.txt", "wb") as file:
         file.write(tag)
-        print("Export of variable tag aka AES256 encrypted text identifier for verification complete. Proceeding with writing out Encrypted message. Returning to Menu.")
+        print("Export of variable tag aka AES192 encrypted text identifier for verification complete. Proceeding with writing out Encrypted message. Returning to Menu.")
 
     # writing encryption out to a file to be stored for decryption
-    with open("aes256Encrypted.txt", "wb") as file:
+    with open("aes192Encrypted.txt", "wb") as file:
         file.write(encrpyted)
-        print("Export of variable encrypted aka AES256 encrypted text complete. Finished with encryption process. Returning to Menu.")
+        print("Export of variable encrypted aka AES192 encrypted text complete. Finished with encryption process. Returning to Menu.")
 
     # # Test block below to return within the class to test the functionality of the AES class
     # # return encrypted message to compare against the decryption to prove its encryption then decryption back to the correct plaintext.
@@ -69,7 +69,7 @@ def decrypt_message(message,key,cipherStore,tag):
         decryptCipher.verify(tag)
         
         # writing encryption out to a file to be stored for decryption
-        with open("aes256Decrypted.txt", "wb") as file:
+        with open("aes192Decrypted.txt", "wb") as file:
             file.write(decrypt)
             print("Tag matches original tag, export of variable decrypt aka AES decrypted text complete. Finished with decryption process. Returning to Menu.")
 
@@ -88,7 +88,7 @@ def decrypt_message(message,key,cipherStore,tag):
 # encrypt_message(msg,key=load_key())
 # print("Encryption complete!")
 # 
-# decrypt_message(message=open("aes256Encrypted.txt", "rb").read(),key=load_key(),cipherStore=open("aes256CipherStore_nonce.txt", "rb").read(), tag=open("aes256EncryptedTag.txt", "rb").read())
+# decrypt_message(message=open("aes192Encrypted.txt", "rb").read(),key=load_key(),cipherStore=open("aes192CipherStore_nonce.txt", "rb").read(), tag=open("aes192EncryptedTag.txt", "rb").read())
 # print("Decryption complete!")
 # 
 # # Testing the pass through capabilities of the AES encryption and decrpyion method and compatibility of saved files for Message, Key, Encrypted message, CipherStore/Nonce, and Tag# 
@@ -97,5 +97,5 @@ gen_key()
 encrypt_message(msg,key=load_key())
 print("Encryption complete!")
 
-decrypt_message(message=open("aes256Encrypted.txt", "rb").read(),key=load_key(),cipherStore=open("aes256CipherStore_nonce.txt", "rb").read(), tag=open("aes256EncryptedTag.txt", "rb").read())
+decrypt_message(message=open("aes192Encrypted.txt", "rb").read(),key=load_key(),cipherStore=open("aes192CipherStore_nonce.txt", "rb").read(), tag=open("aes192EncryptedTag.txt", "rb").read())
 print("Decryption complete!")
